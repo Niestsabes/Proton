b7589f1b8e0fea261b5ea62be2f75ca441975bf0
Neitsabes <sebcayet@gmail.com>
2023-03-25 11:44:32 +0100

    :sparkles: Completed camera control for galaxy

--- Assets/Scripts/Scenes/GameScene/Manager/GameSceneManager.cs
+++ Assets/Scripts/Scenes/GameScene/Manager/GameSceneManager.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class GameSceneManager : MonoBehaviour
 {
+    public static GameSceneManager instance
+    {
+        get { return GameSceneManager._instance; }
+    }
+    private static GameSceneManager _instance;
+
     [Header("Prefabs")]
     [SerializeField] private GameObject galaxyPrefab;
+
+    [Header("GameObjects")]
+    [SerializeField] private GalaxyCameraController cameraController;
 
     private GalaxyObject galaxyObject;
     private GalaxyFactory galaxyFactory = new GalaxyFactory();
@@ -13,8 +22,12 @@ public class GameSceneManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        if (GameSceneManager.instance != null) { Destroy(GameSceneManager.instance); }
+        GameSceneManager._instance = this;
+
         Galaxy galaxy = this.galaxyFactory.GenerateGalaxy(10);
         this.galaxyObject = GalaxyObject.InstantiateObject(galaxy, this.galaxyPrefab);
+        this.cameraController.AttackGalaxyObject(this.galaxyObject);
     }
 
     // Update is called once per frame
