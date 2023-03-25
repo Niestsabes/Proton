22be600416ba816453ccbc509639c144f3a5275d
Neitsabes <sebcayet@gmail.com>
2023-03-25 19:24:06 +0100

    :sparkles: Introduces TextMechPro. Implements Home scene. Updates musics.

--- Assets/Scripts/Scenes/HomeScene/Manager/HomeSceneManager.cs
+++ Assets/Scripts/Scenes/HomeScene/Manager/HomeSceneManager.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HomeSceneManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void NavigateToGame()
     {
-        
+        SceneManager.LoadScene((int)SceneEnum.GAME);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void NavigateToGallery() { }
+
+    public void NavigateToCredit() { }
+
+    public void QuitGame()
     {
-        
-    }
+        Application.Quit();
+    } 
 }
