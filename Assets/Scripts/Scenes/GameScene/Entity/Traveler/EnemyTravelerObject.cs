bab1dd13177182fc5334ebcc78af87e4c5ce9746
Neitsabes <sebcayet@gmail.com>
2023-03-26 14:59:25 +0200

    :truck: Build ready

--- Assets/Scripts/Scenes/GameScene/Entity/Traveler/EnemyTravelerObject.cs
+++ Assets/Scripts/Scenes/GameScene/Entity/Traveler/EnemyTravelerObject.cs
@@ -3,5 +3,8 @@ using UnityEngine;
 
 public class EnemyTravelerObject : TravelerObject
 {
-    
+    public override void AddController(bool isAI)
+    {
+        this.controller = this.gameObject.AddComponent<TravelerControllerAIEnemy>();
+    }
 }
